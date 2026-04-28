0d4edb5dd4b011aa10d16ff98e9ef15b70201929
Mostafa <parisantgerman10@gmail.com>
2026-04-28 23:20:47 +0300

    some improvements on all the code

--- master.js
+++ master.js
@@ -2,7 +2,7 @@ const unreadNotify = document.querySelectorAll("ul.unread li");
 const notifyCount = document.querySelector("h1 .notify-count");
 const markBtn = document.querySelector("header .mark-btn");
 
-markBtn.addEventListener("click", (e) => {
+markBtn.addEventListener("click", () => {
   unreadNotify.forEach((notify) => notify.classList.remove("unread-notify"));
   notifyCount.textContent = "0 ";
   markBtn.disabled = true;
